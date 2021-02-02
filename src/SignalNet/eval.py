c4ea2b6148e0bb8a475845df8d2795308a42bf62
kodeworker <fxp61005@gmail.com>
2021-02-02 14:06:26 +0800

    SignalNet update

--- src/SignalNet/eval.py
+++ src/SignalNet/eval.py
@@ -4,6 +4,9 @@ import numpy as np
 from model import CNN
 from dataset import DatasetFolder, npy_loader
 
+from torch.nn import CrossEntropyLoss
+import numpy as np
+
 if __name__ == "__main__":
     
     print("+++ SignalNet Initialized +++")
@@ -38,9 +41,12 @@ if __name__ == "__main__":
     cnn.load_state_dict(torch.load(save_model_path, device))
     total_step = len(test_loader)
     #print(total_step)
-    loss_list_test = []
+    #loss_list_test = []
     acc_list_test = []
+    
     cnn.eval()
+    criterion = CrossEntropyLoss()
+    results = {}
     
     with torch.no_grad():
         for i, (signals, labels) in enumerate(test_loader):
@@ -49,14 +55,38 @@ if __name__ == "__main__":
             labels=labels.float().to(device)
             outputs = cnn(signals)
             loss = criterion(outputs, labels.long())
-            loss_list_test.append(loss.item())
-            if epoch%10 ==0:
-                print(loss)
+            #loss_list_test.append(loss.item())
+            #if epoch%10 ==0:
+            #    print(loss)
             total = labels.size(0)
             _, predicted = torch.max(outputs.data, 1)
             correct = (predicted == labels.long()).sum().item()
             acc_list_test.append(correct / total)
-            if (epoch) % 1 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-                      .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-                              (correct / total) * 100))
+            
+            labels_arr = labels.detach().cpu().numpy()
+            predict_arr = predicted.detach().cpu().numpy()
+            
+            for pred, true in zip(predict_arr, labels_arr):
+                if true not in results:
+                    results[true] = {"n_total": 1} 
+                    if pred == true:
+                        results[true]["n_correct"] = 1 
+                    else:
+                        results[true]["n_correct"] = 0
+                else:
+                    results[true]["n_total"] += 1 
+                    if pred == true:
+                        results[true]["n_correct"] += 1
+            
+            #print('Loss: {:.4f}, Accuracy: {:.2f}%'
+            #      .format(loss.item(), (correct / total) * 100))
+    
+    n_total_correct = 0
+    n_total_counts = 0
+    for key, value in results.items():
+        print("Lable: {}, Accuracy: {:.2f} %, Counts: {}".format(key, results[key]["n_correct"]/results[key]["n_total"]*100, results[key]["n_total"]))
+        n_total_correct += results[key]["n_correct"]
+        n_total_counts += results[key]["n_total"]
+    print("Total Accuracy: {:.2f} %".format(n_total_correct/n_total_counts*100))
+    
+    
